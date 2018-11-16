0d11f96729bcf3cd99a78448176eb11f8268cf72
Filip Kudliński <s434734@pc02g.labs.wmi.amu.edu.pl>
2018-11-16 12:59:59 +0100

    naprawione

--- src/main/java/DodawanieLib.java
+++ src/main/java/DodawanieLib.java
@@ -1,11 +1,14 @@
 public class DodawanieLib {
 
     public static String dodaj(String a, String b){
+
         if(a.equals("0") && b.equals("0")){
             return "0";
         }else{
-            return "2";
+            return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
         }
+
+
     }
 
 }
