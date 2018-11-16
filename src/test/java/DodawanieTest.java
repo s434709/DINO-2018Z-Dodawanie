f69b846e78e9d952534ccaed5087b52bc57d8c00
Patryk Żywica <bikol@amu.edu.pl>
2018-11-16 12:35:22 +0100

    broke

--- src/test/java/DodawanieTest.java
+++ src/test/java/DodawanieTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 import org.junit.Assert.*;
 
+import java.util.Random;
+
 public class DodawanieTest {
 
     @Test
@@ -11,4 +13,16 @@ public class DodawanieTest {
     public void test2(){
         org.junit.Assert.assertEquals("2", DodawanieLib.dodaj("1","1"));
     }
+
+    @Test
+    public void test3(){
+        Random r = new Random();
+        for(int i = 0;i<100;i++){
+            int a = r.nextInt(1000);
+            int b = r.nextInt(1000);
+
+            org.junit.Assert.assertEquals(Integer.toString(a+b), DodawanieLib.dodaj(Integer.toString(a),Integer.toString(b)));
+        }
+    }
+
 }
