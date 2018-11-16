5a1df61f1be7de3aad43ebb2382370fbe42be771
Filip Kudliński <s434734@pc02g.labs.wmi.amu.edu.pl>
2018-11-16 13:09:32 +0100

    test4passed

--- src/main/java/DodawanieLib.java
+++ src/main/java/DodawanieLib.java
@@ -3,7 +3,7 @@ public class DodawanieLib {
     public static String dodaj(String a, String b){
     	int ai = Integer.parseInt(a);
     	int bi = Integer.parseInt(b);
-        if(ai <= 1000 && bi <= 1000) {
+        if(ai <= 100001 && bi <= 1000) {
         	return String.valueOf(ai+bi);
         }
         return "you're a wizard harry";
